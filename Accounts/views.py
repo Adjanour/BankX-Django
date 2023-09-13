824d58579f30774a5d3b58d1bc9b15e4be65876f
YOUNG KIRK <adjanour@icloud.com>
2023-09-13 07:59:09 +0000

    we have good views now

--- Accounts/views.py
+++ Accounts/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import SignUpForm, LoginForm, AccountCreationForm
 from django.contrib.auth.decorators import login_required
-from .models import Account,User
+from .models import Account
+import uuid
 
 
 def signup(request):
@@ -13,7 +14,7 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}. You can now log in.')
-            return redirect('create_account')
+            return redirect('login')
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
@@ -27,7 +28,7 @@ def login_view(request):
         if user is not None:
                 login(request, user)
                 messages.success(request, f'Welcome back, {username}!')
-                return redirect('account_overview')
+                return redirect('create_account')
         else:
                 messages.error(request, 'Invalid username or password. Please try again.')
     else:
@@ -47,25 +48,18 @@ def index(request):
 def create_account(request):
     if request.method == 'POST':
         # Save the form data to the database.
-        user = User.objects.create(
-            username=request.POST['username'],
-            email=request.POST['email'],
-            password=request.POST['password'],
-            first_name=request.POST['first_name'],
-            last_name=request.POST['last_name'],
-            age=request.POST['age'],
-        )
-
+        user = request.user
         account = Account.objects.create(
             user=user,
+            account_number = generate_id(),
             account_type=request.POST['account_type'],
             initial_deposit=request.POST['initial_deposit'],
             account_pin=request.POST['account_pin'],
             account_balance=request.POST['initial_deposit'],
         )
 
-        # Redirect the user to the success page.
-        return redirect('success')
+        # Redirect the user to the accoun overview page
+        return redirect('account_overview')
 
     else:
         form = AccountCreationForm(request.POST)
@@ -100,3 +94,8 @@ def delete_account(request):
         # Handle account deletion logic here
         return redirect('logout')  # Redirect to logout page after deleting the account
     return render(request, 'delete_account.html')
+
+def generate_id():
+    unique_id = uuid.uuid4()
+    id_str = str(unique_id)
+    return id_str
