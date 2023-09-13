f2d237ff5a73e997bbd04af1fce521f093132f1b
YOUNG KIRK <adjanour@icloud.com>
2023-09-13 07:58:47 +0000

    fixed -at least a little. still needs work

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('account_overview/', views.account_overview, name='account_overview'),
     path('create_account/', views.create_account, name='create_account'),
     path('create_account', views.create_account, name='create_account'),
-    path('deposit/', views.deposit, name='deposit'),
+    path('deposit', views.deposit, name='deposit'),
     path('withdraw/', views.withdraw, name='withdraw'),
     path('delete_account/', views.delete_account, name='delete_account'),
 ]
