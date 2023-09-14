2481dd6ce4b07ac06daea790af92cd1df83d87bc
YOUNG KIRK <adjanour@icloud.com>
2023-09-14 09:01:31 +0000

    updated views and urls to reflect api

--- BankX/settings.py
+++ BankX/settings.py
@@ -37,10 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Accounts'
+    'corsheaders',
+    'rest_framework',
+    'Accounts',
+    'APIx',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
